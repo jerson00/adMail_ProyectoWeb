06b14cc956984282b19b3ed7808bd039c311470b
jerson00 <jerson.00@hotmail.com>
2015-03-31 01:39:33 -0600

    Revert "Login"
    This reverts commit 0ea4648206e9c09e2ab25e487ccf2cae60edd2ef.

--- admail/application/controllers/user.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class User extends CI_Controller {
-
-	public function index()
-	{
-		$this->load->view('user/user_login');
-	}
-}
--- admail/application/controllers/userregister.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-class UserRegister extends CI_Controller {
-
-	public function index()
-	{
-		$this->load->view('user/register');
-	}
-}
